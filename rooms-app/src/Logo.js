2bdd87293bd1c818e8908cc68335aa8c64004a43
ninjasar <sarahpierce@Sarahs-MacBook-Pro-3.local>
2018-06-26 12:24:45 -0400

    old stuff again

---
+++ rooms-app/src/Logo.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+
+import './index.css';
+import torch from './torch.png';
+
+
+
+const Logo = (props) => {
+  return (
+    <div className="logoContainer">
+      <div>
+        <img src={torch} alt="torch"/>
+      </div>
+    </div>
+  )
+};
+
+export default Logo;
+
+
+/*
+var Logo = function (_React$Component) {
+  _inherits(Logo, _React$Component);
+
+  function Logo() {
+    _classCallCheck(this, Logo);
+
+    return _possibleConstructorReturn(this, (Logo.__proto__ || Object.getPrototypeOf(Logo)).apply(this, arguments));
+  }
+
+  _createClass(Logo, [{
+    key: 'render',
+
+
+    //<Route path="*"/>
+    value: function render() {
+      return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+        'div',
+        { className: 'logoContainer', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 15
+          },
+          __self: this
+        },
+        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+          'div',
+          {
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 16
+            },
+            __self: this
+          },
+          __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement('img', { src: __WEBPACK_IMPORTED_MODULE_2__torch_png___default.a, alt: 'torch', className: 'logo', __source: {
+              fileName: _jsxFileName,
+              lineNumber: 17
+            },
+            __self: this
+          })
+        )
+      );
+    }
+  }]);
+
+  return Logo;
+}(__WEBPACK_IMPORTED_MODULE_0_react___default.a.Component);
+
+ __webpack_exports__["a"] = (Logo);*/
---
+++ src/Btn.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {BrowserRouter, NavLink } from 'react-router-dom';
+
+
+import './index.css';
+import ban from './banner.png';
+
+
+
+const Btn = (props) => {
+  return (
+    <div id="homeLayout">
+      <NavLink to="/home/">
+        <button className="helloBtn">
+          hello
+        </button>
+      </NavLink>
+    </div>
+  )
+};
+
+export default Btn;
+
+
+
+/*
+var Btn = function (_React$Component) {
+  _inherits(Btn, _React$Component);
+
+  function Btn(props) {
+    _classCallCheck(this, Btn);
+
+    return _possibleConstructorReturn(this, (Btn.__proto__ || Object.getPrototypeOf(Btn)).call(this, props));
+  }
+
+  _createClass(Btn, [{
+    key: 'render',
+    value: function render() {
+      return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+        'div',
+        { className: 'homeContainer', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 18
+          },
+          __self: this
+        },
+        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_1_react_router_dom__["b"],
+          { to: '/home/', __source: {
+              fileName: _jsxFileName,
+              lineNumber: 19
+            },
+            __self: this
+          },
+          __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+            'button',
+            { className: 'helloBtn', __source: {
+                fileName: _jsxFileName,
+                lineNumber: 20
+              },
+              __self: this
+            },
+            'Hello'
+          )
+        )
+      );
+    }
+  }]);
+
+  return Btn;
+}(__WEBPACK_IMPORTED_MODULE_0_react___default.a.Component);
+
+ __webpack_exports__["a"] = (Btn); */
