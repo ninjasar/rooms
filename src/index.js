62519aaadbdcad648f7c34fa128f5789acb230b1
ninjasar <sarahpierce@Sarahs-MacBook-Pro-3.local>
2018-06-29 15:03:04 -0400

    ugh

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
-registerServiceWorker();
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
-import Amplify from "aws-amplify";
-
-import './index.css';
-import App from './App.js';
-import config from "./config";
-import registerServiceWorker from "./registerServiceWorker";
-
-
 
 ReactDOM.render (
     <Router>
@@ -33,4 +12,3 @@ ReactDOM.render (
     document.getElementById('root'),
   );
   registerServiceWorker();
->>>>>>> master
